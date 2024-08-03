0a7e3729421633b260d70c3fcd90e794eafb7762
ClauCelis <ps.celis@gmail.com>
2024-08-02 23:05:52 -0400

    Listo para entrega

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,9 @@
 import "../../node_modules/@popperjs/core/dist/umd/popper.min.js";
 import "../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
+
+$(document).ready(function () {
+    $("#enviarCorreo").click(function () {
+        alert("Mensaje enviado");
+    });
+});
